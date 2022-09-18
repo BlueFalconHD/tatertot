93fcf34a3841e30db1a2f588932827cb51288531
Hayes Dombroski <72631767+BlueFalconHD@users.noreply.github.com>
2022-09-18 13:27:50 -0500

    added generator script and config.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import config from "../config.json" assert { type: "json" };
+import fs from "fs";
+
+let html = `<!DOCTYPE html>
+<head>
+  <title>${config.page.title}</title>
+  <meta content="${config.embed.title}" property="og:title" />
+  <meta content="${config.embed.description}" property="og:description" />
+  <meta content="${config.embed.url}" property="og:url" />
+  <meta content="${config.embed.image}" property="og:image"/>
+  <meta content="${config.embed.color}" data-react-helmet="true" name="theme-color" />
+  <style>
+    .body { width: 100vw; height: 100vh; padding: 0px; margin: 0px; overflow: hidden;};
+  </style>
+</head>
+
+<body class="body">
+  <script async>
+    setTimeout(() => {
+     window.location.replace("${config.redirect}")
+    }, ${config.delay});
+  </script>
+
+  <iframe src="${config.iframe}" style="box-shadow: none !important; width: 100vw; height: 100vh;"></iframe>
+</body>
+`;
+
+fs.writeFileSync("../index.html", html);
+console.log("🥔 Tatertot Saved");

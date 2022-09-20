8de55234696dc730cc232b398174e1d65ad6d484
Blue <72631767+BlueFalconHD@users.noreply.github.com>
2022-09-20 13:54:42 -0500

    😜 Finalize taterloader

--- taterloader.js
+++ taterloader.js
@@ -1,9 +1,12 @@
 let url = window.location.href
 url = url.replace("https://", "")
-url = url.replace("github.io", "")
+url = url.replace(".github.io", "")
 console.log(url)
 
 function alerts() {
   console.log('🥔 Taters are delicious!')
-  console.log('⚙️ Config Loaded From: https://github.com/' + url + "/blo" )
+  console.log('⚙️ Config Loaded From: https://github.com/' + url + "blob/master/config.json" )
+  console.log('🍴 Forked from https://github.com/bluefalconhd/tatertot')
 }
+
+setTimeout(alert, 10)
